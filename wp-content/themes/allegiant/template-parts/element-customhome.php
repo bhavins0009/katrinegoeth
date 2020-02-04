a1ba142a73c1e952a5d4f71b2d996ffec71fe312
unknown <ds@softinform.dk>
2020-02-04 18:18:07 +0530

    Design changes of home page

--- wp-content/themes/allegiant/template-parts/element-customhome.php
+++ wp-content/themes/allegiant/template-parts/element-customhome.php
@@ -51,5 +51,22 @@
 			</div>
 		</div>
 	</div>
+	<div class="width-col">
+		<div class="image-wrapper">
+			<div class="img-box">
+				<img alt="" class="mySlides_4" src="<?php bloginfo('template_url'); ?>/images/03.jpg" style="display: block">
+				<img alt="" class="mySlides_44" src="<?php bloginfo('template_url'); ?>/images/02.jpg" style="display: none">
+				<img alt="" class="mySlides_4" src="<?php bloginfo('template_url'); ?>/images/01.jpg" style="display: none">
+				<div class="img-control">
+					<a href="javascript:;" class="sl" onclick="plusDivs(-1, 'mySlides_4')"></a>
+                	<a href="javascript:;" class="sr"onclick="plusDivs(1, 'mySlides_4')"></a>
+                </div>
+			</div>
+			<div class="img-detail">
+				<h5>Title</h5>
+				<span>1/5</span>
+			</div>
+		</div>
+	</div>
 </div>
 
